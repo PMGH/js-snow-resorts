efcc2237c266b2778b72fbc515fbb717e07ec010
Peter McCready <petermccready2@gmail.com>
2017-12-09 20:43:58 +0000

    Adds ski area by region pie chart

--- public/app.js
+++ public/app.js
@@ -10,28 +10,15 @@ var resortIndexRequest = function(){
   makeRequest(url, resortIndexRequestComplete);
 }
 
-var weatherRequest = function(coords, days){
-  var token = "ebbbfe3e5f59416284e222010170812";
-  var lat = coords.lat;
-  var long = coords.lng;
-  var num_days = days;
-  var url = "https://api.worldweatheronline.com/premium/v1/ski.ashx?key=" + token + "&q=" + lat + "," + long + "&num_of_days=" + num_days + "&includeLocation=no&format=json";
-  makeRequest(url, weatherRequestComplete);
-}
-
-var weatherRequestComplete = function(){
-  if (this.status != 200) return;
-  var jsonString = this.responseText;
-  var weatherData = JSON.parse(jsonString);
-  console.log(weatherData);
-}
-
 var resortIndexRequestComplete = function(){
   if (this.status != 200) return;
   var jsonString = this.responseText;
   var resortsData = JSON.parse(jsonString);
+  console.log(resortsData);
   var regions = getRegions(resortsData);
   var regionSelect = document.getElementById('region-select');
+
+  pieChartSkiAreaByRegion(resortsData, regions);
 
   populateRegionSelector(regions);
   regionSelect.addEventListener('change', function(){
@@ -134,13 +121,93 @@ var populateResorts = function(skiAreas){
   });
 }
 
+
+
+var weatherRequest = function(coords, days){
+  var token = "ebbbfe3e5f59416284e222010170812";
+  var lat = coords.lat;
+  var long = coords.lng;
+  var num_days = days;
+  var url = "https://api.worldweatheronline.com/premium/v1/ski.ashx?key=" + token + "&q=" + lat + "," + long + "&num_of_days=" + num_days + "&includeLocation=no&format=json";
+  makeRequest(url, weatherRequestComplete);
+}
+
+var weatherRequestComplete = function(){
+  if (this.status != 200) return;
+  var jsonString = this.responseText;
+  var weatherData = JSON.parse(jsonString);
+  console.log(weatherData);
+}
+
+
+
+var pieChartSkiAreaByRegion = function(apiData, regions){
+  var main = document.getElementById('page-main');
+  var id = 'region-pie-chart';
+
+  var chartDiv = document.createElement('div');
+  chartDiv.id = id;
+  main.appendChild(chartDiv);
+
+  var container = document.querySelector('#' + id);
+  var title = 'Ski Areas by Region';
+  var series = [{
+    name: 'Ski Areas by region',
+    data: []
+  }];
+
+  // get region objects
+  var regionObjects = [];
+  apiData.forEach(function(item){
+    if (item.Region[0] != undefined){
+      var region = {
+        name: item.Region[0].name,
+        y: 1
+      }
+      regionObjects.push(region);
+    }
+  });
+  console.log("Regions: ", regions);
+  console.log("Region Objects: ", regionObjects);
+
+  // count occurrences
+  var counts = [];
+  for (var region of regions){
+    var count = {
+      name: region,
+      y: 0
+    }
+    counts.push(count);
+  }
+
+  for (var count of counts){
+    for (var object of regionObjects){
+      if (count.name === object.name){
+        count.y += 1;
+      }
+    }
+  }
+  console.log("Counts: ", counts);
+
+  // push to series data
+  counts.forEach(function(count){
+    series[0].data.push(count);
+  });
+  console.log("Series: ", series);
+
+
+  // create pie chart
+  new PieChart(container, title, series);
+
+}
+
+
+
 var removeChildNodes = function(node){
   while (node.hasChildNodes()) {
     node.removeChild(node.lastChild);
   }
 }
-
-
 
 var app = function(){
   resortIndexRequest();
