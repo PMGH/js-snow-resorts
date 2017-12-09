9451290fd270f7e03bf82b4b180da95b8c7d149e
Peter McCready <petermccready2@gmail.com>
2017-12-09 21:45:15 +0000

    Adds mapWrapper prototypes

--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -1,7 +1,48 @@
-var mapWrapper = function(container, coords, zoom){
+var MapWrapper = function(container, coords, zoom){
   this.googleMap = google.map.Map(container, {
     center: coords,
     zoom: zoom
   });
   this.markers = [];
+}
+
+MapWrapper.prototype.addMarker = function(coords){
+  var marker = new google.maps.Marker({
+    position: coords,
+    icon: "/icons/gnss.png",
+    infoWindowOpen: false,
+    map: this.googleMap
+  });
+
+  this.markers.push(marker);
+  marker.infoWindow = new google.maps.InfoWindow({
+    content: `${marker.position}`  // resort name and location
+  });
+
+  marker.addListener('click', function(){
+    for (var mark of this.markers){
+      if (mark.infowindowOpen){
+        mark.infowindow.close();
+      }
+    }
+    marker.infowindow.open(this.googleMap, marker);
+    marker.infowindowOpen = true;
+  }.bind(this));
+
+}
+
+MapWrapper.prototype.centerFunction = function(coords){
+  this.googleMap.setCenter(coords);
+  this.googleMap.setMapTypeId('hybrid');
+  this.googleMap.setZoom(10);
+}
+
+MapWrapper.prototype.whereAmI = function(){
+  navigator.geolocation.getCurrentPosition(function(position){
+    var coords = {lat: position.coords.latitude, lng: position.coords.longitude};
+    this.googleMap.setCenter(coords);
+    this.googleMap.setMapTypeId('hybrid');
+    this.googleMap.setZoom(19);
+    this.addMarker(coords);
+  }.bind(this));
 }
